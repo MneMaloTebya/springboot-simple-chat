c4cae1605a9d5436151c9644a6a113412e2bb0e7
Alexander <LipatovAlexander93@yandex.ru>
2022-11-03 15:17:09 +0300

    added gitignore and skeleton ti the project

---
+++ src/main/java/com/github/mnemalotebya/spingbootsimplechat/GroupChatApplication.java
@@ -0,0 +1,13 @@
+package com.github.mnemalotebya.spingbootsimplechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupChatApplication.class, args);
+	}
+
+}
